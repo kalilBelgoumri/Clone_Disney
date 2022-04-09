c5350f08a88ef5afd586771f515f6d733e89eee1
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-04-09 13:13:36 +0000

    add logo and background

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Head from "next/head";
+import Image from "next/image";
+import background from "../assets/disney1_background.png";
+import logo from "../assets/logo.svg";
 
-function Layout() {
+export default function Layout({ children, title }) {
   return (
     <div>
       <Head>
-        <title>{pageTitle}</title>
+        <title>{title}</title>
       </Head>
+      <div className="bg-gray-200 w-full h-32 shadow-2xl ">
+        <div className="logo flex">
+          <Image src={logo} alt="logo" />
+        </div>
+      </div>
+      <Image
+        className="absolute top-0"
+        src={background}
+        priority="responsive"
+        alt="background"
+      />
       <main>{children}</main>
     </div>
   );
 }
-
-export default Layout;

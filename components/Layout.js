1d6ada3da09bc3cdea6f084ac5c9257024ba3adf
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-04-09 13:55:46 +0000

    add useRouter

--- components/Layout.js
+++ components/Layout.js
@@ -4,18 +4,32 @@ import Image from "next/image";
 import background from "../assets/disney1_background.png";
 import background_down from "../assets/download.svg";
 import logo from "../assets/logo.svg";
+import { useRouter } from "next/router";
 
 export default function Layout({ children, title }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
         <title>{title}</title>
       </Head>
-      <div className=" w-full h-30  shadow-2xl ">
-        <div className="logo flex justify-center">
-          <Image src={logo} alt="logo" priority="responsive" width={160} />
+      {/* Background */}
+      <div className=" w-full h-30 shadow-2xl ">
+        {/* Logo */}
+        <div className="logo flex justify-center ">
+          <Image
+            className="cursor-pointer"
+            src={logo}
+            alt="logo"
+            priority="responsive"
+            width={160}
+            onClick={() => router.push("/")}
+          />
         </div>
+        {/* Background_down */}
+        <div className=" h-[8.5vh] border-t-gray border-[1px]"></div>
       </div>
+      {/* Background_cover */}
       <Image src={background} priority="responsive" alt="background" />
       <div className="">
         <Image

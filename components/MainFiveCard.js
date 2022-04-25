9f50273acae0cc7fa1571dabc8c1a6186f0af5f3
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-04-25 23:02:53 +0200

    update mainCard

--- components/MainFiveCard.js
+++ components/MainFiveCard.js
@@ -7,47 +7,76 @@ import { Container } from "@mui/material";
 export default function MainFiveCard() {
   return (
     <>
-      <span className="mt-20 text-center text-[24px] text-[#202249]">
+      <span className="mt-20 text-center text-[24px] text-[#202249] hover:shadow-2xl hover:transition-all">
         Planifiez une journée qui vous ressemble
       </span>
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          "& > :not(style)": {
+      <div className="mt-5 flex items-center justify-center">
+        <Box
+          sx={{
             display: "flex",
-            alignItems: "center",
-            width: 340,
-            height: 130,
-            borderRadius: "12px",
-          },
-        }}
-      >
-        <Paper elevation={4}>
-          <div className="flex pl-2">
-            <Image
-              className="rounded-lg"
-              src="/../public/small-world-attraction_1-1_zoom.jpg"
-              alt="small"
-              width="170%"
-              height="170%"
-            />
-          </div>
-          <Container maxWidth="xl">
-            <div className="flex flex-col">
-              <div className="flex flex-1 items-start justify-start">
-                <span className="text-[17px] font-bold leading-none text-[#202249]">
-                  Vous venez en couple ou avec des amis ?
+
+            "& > :not(style)": {
+              display: "flex",
+              alignItems: "center",
+              width: 340,
+              height: 130,
+            },
+          }}
+          className="flex flex-col items-center justify-center gap-10 rounded-lg  md:flex-row"
+        >
+          <Paper elevation={4}>
+            <div className="flex pl-2">
+              <Image
+                className="rounded-lg"
+                src="/../public/world_dreams-matter-more-than-ever-key-visual-young-adults.jpg"
+                alt="small"
+                width="170%"
+                height="170%"
+              />
+            </div>
+            <Container maxWidth="xl">
+              <div className="flex flex-col">
+                <div className="flex flex-1 items-start justify-start">
+                  <span className="pb-1 text-[17px] font-bold leading-none text-[#202249]">
+                    Vous venez en couple ou avec des amis ?
+                  </span>
+                </div>
+
+                <span className="font-sans text-[15px]  leading-tight text-[#202249]">
+                  Attractions, restaurants, boutiques... Planifiez une journée
+                  qui vous ressemble !
                 </span>
               </div>
-              <span className="font-sans text-[15px]  leading-tight text-[#202249]">
-                Attractions, restaurants, boutiques... Planifiez une journée qui
-                vous ressemble !
-              </span>
+            </Container>
+          </Paper>
+
+          <Paper elevation={4}>
+            <div className="flex pl-2">
+              <Image
+                className="rounded-lg"
+                src="/../public/small-world-attraction_1-1_zoom.jpg"
+                alt="small"
+                width="170%"
+                height="170%"
+              />
             </div>
-          </Container>
-        </Paper>
-      </Box>
+            <Container maxWidth="xl">
+              <div className="flex flex-col">
+                <div className="flex flex-1 items-start justify-start">
+                  <span className="pb-1 text-[17px] font-bold leading-none text-[#202249]">
+                    Vous avez un enfant de 0 à 6 ans ?
+                  </span>
+                </div>
+
+                <span className="font-sans text-[15px]  leading-tight text-[#202249]">
+                  Préparez cette journée d`aventure et de rêve, pour plaire à
+                  toute la famille !
+                </span>
+              </div>
+            </Container>
+          </Paper>
+        </Box>
+      </div>
     </>
   );
 }

d55f7644c8889681624033c3f2f071b3ece8ebdd
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-04-26 15:11:43 +0200

    update

---
+++ components/main/MainSix.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MainSix() {
+  return (
+    <>
+      <span className=" text-[24px] text-[#202249]">
+        Prolongez la magie jusque dans vos rêves
+      </span>
+      <p className=" mx-auto w-[816px] font-sans text-[18px] text-[#202249]">
+        Téléportez-vous dans l’ambiance dépaysante de nos hôtels à thèmes et
+        bénéficiez de l’accès aux Parcs avant l’heure d’ouverture, de Points
+        Selfie à l’hôtel et autres avantages uniques.
+      </p>
+    </>
+  );
+}
+
+export default MainSix;
---
+++ data/DatasMainSix.js
@@ -0,0 +1,49 @@
+import Image from "next/image";
+
+const datasMainSix = [
+  {
+    _id: "1",
+    title: `Disney’s Hotel New York - The Art of Marvel ★★★★`,
+    description: `Vivez comme un Super Héros dans ce décor typiquement new-yorkais.`,
+    url: "../public/mainSix/1.jpg",
+    pieton: `10 min a pied des Parcs`,
+    navette: `8 min en navette gratuite`,
+    btn: `Découvrir l'hotêl`,
+  },
+  {
+    _id: "2",
+    title: `Disney’s Newport Bay Club ★★★★ `,
+    description: `Réveillez-vous dans une ambiance maritime, décorée de clins d'œil aux aventures nautiques de Mickey.`,
+    url: "../public/mainSix/2.jpg",
+    pieton: `15 min a pied des Parcs`,
+    navette: `8 min en navette gratuite`,
+    btn: `Découvrir l'hotêl`,
+  },
+  {
+    _id: "3",
+    title: `Disney’s Sequoia Lodge ★★★ `,
+    description: `Relaxez-vous dans ce lodge boisé, agrémenté de subtiles références à Bambi.`,
+    url: `../public/mainSix/3.jpg`,
+    pieton: `15 min a pied des Parcs`,
+    navette: `8 min en navette gratuite`,
+    btn: `Découvrir l'hotêl`,
+  },
+  {
+    _id: "4",
+    title: `Disney’s Hotel Cheyenne ★★★`,
+    description: `Décompressez avec Woody et Jessie dans l'ambiance animée de cet hôtel à thème Far West.`,
+    url: `../public/mainSix/4.jpg`,
+    pieton: `20 min a pied des Parcs`,
+    navette: `8 min en navette gratuite`,
+    btn: `Découvrir l'hotêl`,
+  },
+  {
+    _id: "5",
+    title: `Découvrez toute la gamme`,
+    description: `Le Parc Walt Disney Studios devient LE lieu où célébrer la diversité en famille ou entre amis ! Laissez-vous transporter par une ambiance magique : concerts live, parade multicolore, attractions ouvertes jusqu'à 2h du matin... C'est parti pour une 2ème édition fabuleuse aux couleurs de l’arc-en-ciel !`,
+    url: `../public/mainSix/5.jpg`,
+    btn: `Découvrir l'hotêl`,
+  },
+];
+
+export default datasMainSix;
